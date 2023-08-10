6a2f4b670bf27408651a4e2c50280f46bd4609dd
Debashis26 <deb.office01@gmail.com>
2023-08-10 13:34:05 +0530

    test case for step 5 added

--- src/index.test.ts
+++ src/index.test.ts
@@ -5,29 +5,32 @@ describe("test add function",()=>{
     it("should exist",()=>{
         expect(add).toBeTruthy()
     })
-    it("shoul return 0 if passed value is empty string",()=>{
+    it("should return 0 if passed value is empty string",()=>{
         expect(add("")).toBe(0)
     })
-    it("shoul return sum of two number",()=>{
+    it("should return sum of two number",()=>{
         expect(add("1")).toBe(1)
     })
-    it("shoul return sum of two number",()=>{
+    it("should return sum of two number",()=>{
         expect(add("2")).toBe(2)
     })
-    it("shoul return sum of two number",()=>{
+    it("should return sum of two number",()=>{
         expect(add("1,2")).toBe(3)
     })
-    it("shoul return sum of two number",()=>{
+    it("should return sum of two number",()=>{
         expect(add("3,2")).toBe(5)
     })
-    it("shoul return sum of n number",()=>{
+    it("should return sum of n number",()=>{
         expect(add("3,2,1")).toBe(6)
     })
-    it("shoul return sum of n number",()=>{
+    it("should return sum of n number",()=>{
         expect(add("1\n2,3")).toBe(6)
     })
-    it("shoul return sum of n number",()=>{
+    it("should return sum of n number",()=>{
         expect(add("//;\n1;2")).toBe(3)
+    })
+    it("should return sum of n number",()=>{
+        expect(add("//-15;\n-11;2")).toBe(13)
     })
 
     
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,17 @@
 export function add(numbers:string){
     if(numbers==="")
     return 0;
-    let regex=/[0-9]/g;
-    const arrayOfNum=numbers.match(regex)?.map(Number) || [];;
-    return arrayOfNum.reduce((sum, num) => sum + num, 0);
+    const negativeRegex=/-\d+/g;
+    let arrayOfNum:number[]=[]
+    arrayOfNum=numbers.match(negativeRegex)?.map(Number) || [];
+    if(arrayOfNum.length==1){
+        throw new Error('negatives not allowed')
+    }else if(arrayOfNum.length>1){
+        throw new Error(`negatives not allowed. The nnegative numbers are ${arrayOfNum}`)
+    }else{
+        let regex=/-?\d+/g;
+        arrayOfNum=numbers.match(regex)?.map(Number) || [];
+        return arrayOfNum.reduce((sum, num) => sum + num, 0);
+    }
+    
 }
